2399e45d777bcdae966c21801c85a6ad2f81288a
hsaeed3 <hsaeed3@student.ohlone.edu>
2024-09-08 06:11:04 -0700

    Doc Update

--- test.py
+++
@@ -1,9 +0,0 @@
-from zyx import completion
-
-response = completion(
-    "Write a story about a man who travels through time to save his family from a deadly disease.",
-    optimize = "costar"
-)
-
-print(response.choices[0].message.content)
-
