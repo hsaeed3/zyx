faebc9e2051ba54968d98bfda18e4c0856df7a2e
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-03 02:32:59 -0800

    [#6] ✨ `CREATE` - init `zyx.lib` for internal & core level resources

--- zyx/lib/__init__.py
+++ zyx/lib/__init__.py
@@ -5,6 +5,4 @@ __all__ = ["utils"]
 
 
 # utils is the only module exported at the init level of `zyx.lib`
-from ._utils import Utils
-
-utils = Utils()  # type: ignore
+from . import _utils as utils
