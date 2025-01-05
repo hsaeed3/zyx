8c4e57a38233ed005a0416b9399e3afca922c5a6
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-05 02:30:59 -0800

    [#1] ✨ `CREATE` - init `zyx` base package w/ placeholder annotation docstring

---
+++ zyx/__init__.py
@@ -0,0 +1,23 @@
+"""
+zyx
+
+A Python library for `effortlessly` creating and managing complex Generative AI
+applications & pipelines, with a **specification on controllable, on-message & autonomous
+AI agents**, all through an incredibly non-complex & comfortable API. `zyx` is designed 
+to be hyper-lightweight, with it's only `main` dependencies being `litellm` & `qdrant-client`.
+
+`zyx` was built with speed, simplicity & developer (my own) experience in mind, and is most easily 
+usable straight from the top:
+
+---
+
+*Chat Completions w/ Easy Structured Outputs & Automatic Tool Calling*
+
+```python
+import zyx
+
+response = zyx.create.completion("hi")
+```
+"""
+
+
