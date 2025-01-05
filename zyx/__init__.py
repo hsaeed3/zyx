432802977610b9e377b01567b81b6befe43d50d2
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-05 03:02:32 -0800

    🐶 `RUFF FORMAT`

--- zyx/__init__.py
+++ zyx/__init__.py
@@ -38,12 +38,11 @@ __all__ = [
 
 # [Core Utils & Flags]
 from . import _utils as utils
+
 # Retrieve Flags, so that if they are set here, they are set globally as well
 global zyx_verbose
 global zyx_debug
-zyx_verbose : bool = utils.zyx_verbose
+zyx_verbose: bool = utils.zyx_verbose
 """Modules will provide printed console outputs & simple information."""
-zyx_debug : bool = utils.zyx_debug
+zyx_debug: bool = utils.zyx_debug
 """Modules will provide extensive & detailed debug information."""
-
-
