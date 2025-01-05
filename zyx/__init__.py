4261cc47f95c7d55da97ece79d2898457aa2fd5a
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-05 02:45:34 -0800

    [#6] ✨ `CREATE` - create `zyx/_utils` for logging/config & add to `zyx/__init__.py`

--- zyx/__init__.py
+++ zyx/__init__.py
@@ -2,11 +2,11 @@
 zyx
 
 A Python library for `effortlessly` creating and managing complex Generative AI
-applications & pipelines, with a **specification on controllable, on-message & autonomous
-AI agents**, all through an incredibly non-complex & comfortable API. `zyx` is designed 
-to be hyper-lightweight, with it's only `main` dependencies being `litellm` & `qdrant-client`.
+applications & pipelines, with a `specification on controllable, on-message & autonomous
+AI agents`, all through an incredibly non-complex & comfortable API. `zyx` is designed
+to be hyper-lightweight, with its only `main` dependencies being `litellm` & `qdrant-client`.
 
-`zyx` was built with speed, simplicity & developer (my own) experience in mind, and is most easily 
+`zyx` was built with speed, simplicity & developer (my own) experience in mind, and is most easily
 usable straight from the top:
 
 ---
@@ -18,6 +18,32 @@ import zyx
 
 response = zyx.create.completion("hi")
 ```
+
+---
+
+hammad saeed | 2025
 """
+
+__all__ = [
+    # [Core Utils & Flags]
+    "utils",
+    "zyx_verbose",
+    "zyx_debug",
+]
+
+
+# ===================================================================
+# [Imports]
+# ===================================================================
+
+# [Core Utils & Flags]
+from . import _utils as utils
+# Retrieve Flags, so that if they are set here, they are set globally as well
+global zyx_verbose
+global zyx_debug
+zyx_verbose : bool = utils.zyx_verbose
+"""Modules will provide printed console outputs & simple information."""
+zyx_debug : bool = utils.zyx_debug
+"""Modules will provide extensive & detailed debug information."""
 
 
