9c12e248c98ccbb27b750d16061eaeceb8237078
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-05 02:52:17 -0800

    [#6] ✨ `CREATE` - create `zyx/core/__init__.py` for all core graph/completion resource

---
+++ zyx/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+zyx.core
+
+This module contains the core functionality for the modules in the `zyx` library.
+Provides the base API for `zyx` chat completions, multimodal generations & the graph
+API for creating agentic workflows & pipelines.
+"""
