7412e14079119c434e7f9659d72a76f5a2248d55
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-05 18:00:17 -0800

    [#6] ✨ `CREATE` - create `zyx/core/__init__.py` for all core resource

---
+++ zyx/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+zyx.core
+
+This module contains the core functionality for the modules in the `zyx` library.
+
+Provides core logging, config, as well as the base API for `zyx` chat completions, multimodal generations & the graph
+API for creating agentic workflows & pipelines.
+"""
