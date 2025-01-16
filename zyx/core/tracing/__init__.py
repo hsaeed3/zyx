84e5bee950b240b909434a41033764f024b4fd60
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-16 15:32:29 -0800

    [#6] 😃 `ADD` - move `.tracing` level modules to init level for `zyx.core.tracing`

--- zyx/core/tracing/__init__.py
+++ zyx/core/tracing/__init__.py
@@ -3,3 +3,13 @@
 
 Provides the application for the tracing (verbose logging) system in zyx.
 """
+
+# tracing toggle & hooks
+from .tracer import trace, on, emit
+
+# debug toggle helper
+from .visualizer import debug
+
+# base class for other zyx modules to inherit from (this is how modules in zyx are traced)
+# they do not directly import the tracer
+from .traceable import Traceable
