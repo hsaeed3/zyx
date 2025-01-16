91bb1b4bc51e0477eca7d578dcccda8d8510ea0d
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-16 03:57:14 -0800

    [#6] ✨ `CREATE` - init & scaffold `zyx/core/tools`

---
+++ zyx/core/tools/__init__.py
@@ -0,0 +1,6 @@
+"""
+### 🧰 zyx.core.tools
+
+Contains the tools registry, which is a global library singleton,
+as well as connected utilties for function calling.
+"""
---
+++ zyx/core/tools/registry.py
@@ -0,0 +1,9 @@
+"""
+### 🧰 zyx.core.tools.registry
+
+Contains the tools registry, which is a global library singleton,
+as well as connected utilties for function calling.
+
+Note, the registry's main functionality revolves only around python
+functions passed as tools.
+"""
