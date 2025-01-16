28222d59fdf9ff506facaec1e1e5fd7c3162cf7c
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-01-16 03:33:14 -0800

    [#6] ✨ `CREATE` - init `zyx/core`

---
+++ zyx/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+## zyx.core
+
+Contains base classes, types, utilities and other foundational components used at a core level
+in the `zyx` package.
+Both the chat completions & graph API are built on top of these core components.
+"""
