fe77f45bee24f3dd15b91faffcc01b587c2e7b11
hsaeed3 <hammadaidev@gmail.com>
2025-08-27 18:56:43 -0700

    chore(refactor): clear legacy modules & tests

--- zyx/core/tracing/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-### 🔎 zyx.core.tracing
-
-Provides the application for the tracing (verbose logging) system in zyx.
-Nothing from here is meant to be used directly, unles you would like to register your own
-modules & events with the tracing system.
-"""
