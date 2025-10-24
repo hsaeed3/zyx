db8f25727b2b7e4ec0fc75dff98875674ca4916f
hsaeed3 <hammadaidev@gmail.com>
2025-10-24 00:51:30 -0700

    chore: clean repo structure

--- zyx/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-## zyx.core
-
-Contains base classes, types, utilities and other foundational components used at a core level
-in the `zyx` package.
-Both the chat completions & graph API are built on top of these core components.
-"""
